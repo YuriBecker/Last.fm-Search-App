5c45f7c887728f701fd61174b437dd22582b8dd7
Yuri Becker <becker.yr@gmail.com>
2020-01-24 21:47:12 -0300

    Add firebase service

---
+++ src/services/Firebase/index.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+const Firebase = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+});
+
+export default Firebase;
+
+export const logout = () => Firebase.auth().signOut();
+
+export const login = (email, password) =>
+  Firebase.auth().signInWithEmailAndPassword(email, password);
+
+export const signUp = (email, password) =>
+  Firebase.auth().createUserWithEmailAndPassword(email, password);
