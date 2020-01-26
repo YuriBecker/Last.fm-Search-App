f673a081a74876b4601c21f1dee943d1af5a4d9b
Yuri Becker <becker.yr@gmail.com>
2020-01-26 00:30:12 -0300

    Update routes

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { Login, SignUp, Search, ResultAlbum, ResultArtist, Error404 } from '../screens';
+import { Login, SignUp, Search, ResultAlbum, ResultArtist, Error404, AlbumInfo } from '../screens';
 import PrivateRoute from './PrivateRoute';
 
 const AppRouter = () => {
@@ -31,6 +31,13 @@ const AppRouter = () => {
           exact
           path="/album"
           component={ResultAlbum}
+          isAuthenticated={isAuthenticated}
+          isVerifying={isVerifying}
+        />
+        <PrivateRoute
+          exact
+          path="/albumInfo"
+          component={AlbumInfo}
           isAuthenticated={isAuthenticated}
           isVerifying={isVerifying}
         />
