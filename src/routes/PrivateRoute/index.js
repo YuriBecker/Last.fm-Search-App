5db81637aed29d94395fce90f89ba02292f33a85
Yuri Becker <becker.yr@gmail.com>
2020-01-26 15:22:07 -0300

    Add redux persist provider

--- src/routes/PrivateRoute/index.js
+++ src/routes/PrivateRoute/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import LoadingUserInfo from '../../screens/Loading/LoadingUserInfo';
+import { Loading } from '../../screens';
 
 const PrivateRoute = ({ component: Component, isAuthenticated, isVerifying, ...rest }) => (
   <Route
     {...rest}
     render={props => {
       if (isAuthenticated) return <Component {...props} />;
-      if (isVerifying) return <LoadingUserInfo />;
+      if (isVerifying) return <Loading />;
       return (
         <Redirect
           to={{
--- src/routes/Root.js
+++ src/routes/Root.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { ToastNotification } from '../components';
 import ThemeProvider from '../providers/ThemeProvider';
 import configureStore from '../store';
 import Routes from './Routes';
+import Loading from '../screens/Loading/Loading';
 
-const store = configureStore();
+const { store, persistor } = configureStore();
 
 const AppRouter = () => {
   return (
     <ThemeProvider>
       <Provider store={store}>
-        <ToastNotification />
-        <Routes />
+        <PersistGate loading={<Loading />} persistor={persistor}>
+          <ToastNotification />
+          <Routes />
+        </PersistGate>
       </Provider>
     </ThemeProvider>
   );
