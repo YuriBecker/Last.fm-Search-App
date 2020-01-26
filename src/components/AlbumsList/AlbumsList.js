359d3a64a12c17dece7f7bc6e1370c9167107e66
Yuri Becker <becker.yr@gmail.com>
2020-01-26 02:12:02 -0300

    Add conditional render

--- src/components/AlbumsList/AlbumsList.js
+++ src/components/AlbumsList/AlbumsList.js
@@ -5,22 +5,32 @@ import PropTypes from 'prop-types';
 import { Tooltip } from '@material-ui/core';
 
 const AlbumsList = ({ albums, onClickAlbum }) => {
-  return albums.map(album => (
-    <Tooltip key={album.name} title={album.name}>
-      <img
-        src={album.image[1]['#text']}
-        alt={album.name}
-        width={80}
-        height={80}
-        style={{ margin: '3px', cursor: 'pointer' }}
-        onClick={() => onClickAlbum(album.mbid)}
-      />
-    </Tooltip>
-  ));
+  return albums.map(
+    album =>
+      // Verify to avoid albums without image
+      album.image[1]['#text'] && (
+        <Tooltip key={album.name} title={album.name}>
+          <img
+            src={album.image[1]['#text']}
+            alt={album.name}
+            width={80}
+            height={80}
+            style={{ margin: '3px', cursor: 'pointer' }}
+            onClick={() => onClickAlbum(album.artist.name, album.name)}
+          />
+        </Tooltip>
+      ),
+  );
 };
 
 AlbumsList.propTypes = {
-  albums: PropTypes.array.isRequired,
+  albums: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      image: PropTypes.array.isRequired,
+      artist: PropTypes.object.isRequired,
+    }),
+  ).isRequired,
   onClickAlbum: PropTypes.func.isRequired,
 };
 
