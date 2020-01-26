fec57103914a8b8468a9a5257db0ebac378b21e5
Yuri Becker <becker.yr@gmail.com>
2020-01-26 15:22:25 -0300

    Add persist setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,29 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './Ducks';
 import rootSaga from './Sagas/rootSaga';
 import { actions as authActions } from './Ducks/auth';
 
-const sagaMiddleware = createSagaMiddleware();
-
 export default function configureStore() {
-  const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
-  /** run saga watchers */
+  const persistConfig = {
+    key: 'root',
+    whitelist: ['history'],
+    storage,
+  };
+
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
+  const sagaMiddleware = createSagaMiddleware();
+  const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
+  const persistor = persistStore(store);
+
+  // Run saga watchers
   sagaMiddleware.run(rootSaga);
+
+  // Verify user auth
   store.dispatch(authActions.verifyRequest());
-  return store;
+
+  return { store, persistor };
 }
