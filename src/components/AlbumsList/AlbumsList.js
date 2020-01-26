e18e7ec7469f42ea60eac8d13743dc4dcc3949ee
Yuri Becker <becker.yr@gmail.com>
2020-01-26 00:30:05 -0300

    update component albumList

--- src/components/AlbumsList/AlbumsList.js
+++ src/components/AlbumsList/AlbumsList.js
@@ -2,25 +2,18 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
 import { Tooltip } from '@material-ui/core';
-import { actions as searchAlbumActions } from '../../store/Ducks/searchAlbum';
 
-const AlbumsList = ({ albums }) => {
-  const dispatch = useDispatch();
+const AlbumsList = ({ albums, onClickAlbum }) => {
   return albums.map(album => (
-    <Tooltip
-      key={album.name}
-      title={album.name}
-      onClick={() => dispatch(searchAlbumActions.searchAlbum(album.name))}
-    >
+    <Tooltip key={album.name} title={album.name}>
       <img
         src={album.image[1]['#text']}
         alt={album.name}
         width={80}
         height={80}
         style={{ margin: '3px', cursor: 'pointer' }}
-        onClick={() => dispatch(searchAlbumActions.searchAlbum(album.name))}
+        onClick={() => onClickAlbum(album.mbid)}
       />
     </Tooltip>
   ));
@@ -28,6 +21,7 @@ const AlbumsList = ({ albums }) => {
 
 AlbumsList.propTypes = {
   albums: PropTypes.array.isRequired,
+  onClickAlbum: PropTypes.func.isRequired,
 };
 
 export default AlbumsList;
